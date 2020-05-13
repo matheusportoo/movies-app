ee6b63c03cb839cd6e88d15eb844a09dba340b13
Matheus Porto <matheusportoo@gmail.com>
2020-05-12 23:44:44 -0300

    fix: adjustment in theme control to get theme selected from session storage

--- front-end/src/components/ThemeControl/index.js
+++ front-end/src/components/ThemeControl/index.js
@@ -3,13 +3,17 @@ import React from 'react'
 import * as S from './style'
 
 const html = document.querySelector('html')
+const themeSelected = sessionStorage.getItem('theme')
 const themeColors = ['dark', 'light']
 const icons = {
   dark: 'night',
   light: 'sunny'
 }
 
-const changeThemeColor = (theme) => html.setAttribute('data-theme', theme)
+const changeThemeColor = (theme) => {
+  html.setAttribute('data-theme', theme)
+  sessionStorage.setItem('theme', theme)
+}
 
 const Button = ({ theme }) => (
   <S.Button
@@ -20,12 +24,16 @@ const Button = ({ theme }) => (
   </S.Button>
 )
 
-const ThemeControl = () => (
-  <div className="c-header__chage-theme-controls">
-    { themeColors.map(theme => (
-      <Button theme={theme} key={`theme-controls-${theme}`} />
-    )) }
-  </div>
-)
+const ThemeControl = () => {
+  themeSelected && changeThemeColor(themeSelected)
+
+  return (
+    <div className="c-header__chage-theme-controls">
+      { themeColors.map(theme => (
+        <Button theme={theme} key={`theme-controls-${theme}`} />
+      )) }
+    </div>
+  )
+}
 
 export default ThemeControl
