92cd0225e624544381488127e9e3cf9003b87cec
Matheus Porto <matheusportoo@gmail.com>
2020-05-28 20:19:53 -0300

    mod: change placeholder cards to improve perfomance

--- front-end/src/components/CardMovie/index.js
+++ front-end/src/components/CardMovie/index.js
@@ -6,37 +6,28 @@ import { getDateFormated } from '../../tools'
 import AverageGrade from '../AverageGrade'
 import * as S from './style'
 
-function Placeholder() {
+const CardMovie = ({ posterPath, title, href, releaseDate, voteAverage, isFetching }) => {
   return (
-    <S.CardMovie>
-      <S.CardMovieImage />
-      <S.CardMovieInfo />
+    <S.CardMovie isFetching={isFetching}>
+      <NavLink to={href}>
+        <S.CardMovieImage>
+          { posterPath && <picture>
+              <source srcSet={`${IMAGE_PATH('poster2x')}${posterPath}`} media='(min-width: 1170px)' />
+              <img srcSet={`${IMAGE_PATH('poster')}${posterPath}`} alt={title} />
+          </picture>
+          }
+        </S.CardMovieImage>
+
+        <S.CardMovieInfo isFetching={isFetching}>
+          <p>
+            <span>release date:</span><br />
+            <span>{getDateFormated(releaseDate)}</span>
+          </p>
+          <AverageGrade value={voteAverage} />
+        </S.CardMovieInfo>
+      </NavLink>
     </S.CardMovie>
   )
-}
-
-const CardMovie = ({ posterPath, title, href, releaseDate, voteAverage, isFetching }) => {
-  return isFetching
-    ? (<Placeholder />)
-    : (<S.CardMovie>
-        <NavLink to={href}>
-          <S.CardMovieImage>
-            { posterPath && <picture>
-                <source srcset={`${IMAGE_PATH('poster2x')}${posterPath}`} media='(min-width: 1170px)' />
-                <img srcset={`${IMAGE_PATH('poster')}${posterPath}`} alt={title} />
-            </picture>
-            }
-          </S.CardMovieImage>
-
-          <S.CardMovieInfo>
-            <p>
-              <span>release date:</span><br />
-              <span>{getDateFormated(releaseDate)}</span>
-            </p>
-            <AverageGrade value={voteAverage} />
-          </S.CardMovieInfo>
-        </NavLink>
-      </S.CardMovie>)
 }
 
 CardMovie.propTypes = {
