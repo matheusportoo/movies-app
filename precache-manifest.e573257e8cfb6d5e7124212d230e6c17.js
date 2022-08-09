0912f44e96fb303aee83900a891910c8faef8b07
matheusportoo <matheusportoo@gmail.com>
2022-08-09 19:01:50 +0000

    Deploying to gh-pages from @ matheusportoo/movies-app@94ec69f676dc8bfad87620a073422d006754994c 🚀

---
+++ precache-manifest.e573257e8cfb6d5e7124212d230e6c17.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfc8cad055eaac81e04",
+    "url": "0.b6e074ad.iframe.bundle.js"
+  },
+  {
+    "revision": "a252366e437000c59bbde81a9807bcdf",
+    "url": "0.b6e074ad.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "885683c61ecb1d5a78ae",
+    "url": "1.c5cb37d9.iframe.bundle.js"
+  },
+  {
+    "revision": "a39424e470b71aac6122",
+    "url": "2.c8f6dc2b.iframe.bundle.js"
+  },
+  {
+    "revision": "64c3b174abadb88f1945",
+    "url": "6.98a925e3.iframe.bundle.js"
+  },
+  {
+    "revision": "b0e968d796aa5cd7a396045e69ecde11",
+    "url": "6.98a925e3.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "62754d6fe6530f7281fd",
+    "url": "7.f0b4b36c.iframe.bundle.js"
+  },
+  {
+    "revision": "60e62f3f87488fca16bb",
+    "url": "8.d6b26a40.iframe.bundle.js"
+  },
+  {
+    "revision": "aea2b77d0caa05a81606",
+    "url": "9.c0a47b6b.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "9.c0a47b6b.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "9736975a273f0285c1f4b2ffedb9b626",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "0c17d773b0961d46d284",
+    "url": "main.193a80e0.iframe.bundle.js"
+  },
+  {
+    "revision": "98d0a6fd500b67789c4f",
+    "url": "runtime~main.7ee447dc.iframe.bundle.js"
+  },
+  {
+    "revision": "0c17d773b0961d46d284",
+    "url": "static/css/main.0ec1658c.chunk.css"
+  },
+  {
+    "revision": "d43eb968e25e81ce0af629bad1069a19",
+    "url": "static/media/code-brackets.d43eb968.svg"
+  },
+  {
+    "revision": "8104e6f5a95cbe9c9615b47daf881fc7",
+    "url": "static/media/colors.8104e6f5.svg"
+  },
+  {
+    "revision": "3f9a1398f6372bd43806b239c70f4759",
+    "url": "static/media/comments.3f9a1398.svg"
+  },
+  {
+    "revision": "d3dc567bd3e26a031bfd7fab1e765508",
+    "url": "static/media/direction.d3dc567b.svg"
+  },
+  {
+    "revision": "e3812c4ff33e4e7ae9686a7fcfef393f",
+    "url": "static/media/flow.e3812c4f.svg"
+  },
+  {
+    "revision": "5ba97e9be21511d352f0056b1970ce21",
+    "url": "static/media/plugin.5ba97e9b.svg"
+  },
+  {
+    "revision": "3452b916238c37efec707e6979db3556",
+    "url": "static/media/repo.3452b916.svg"
+  },
+  {
+    "revision": "66f4b748553f89f290a3247a913072af",
+    "url": "static/media/stackalt.66f4b748.svg"
+  },
+  {
+    "revision": "aeda2db0c9942c728560",
+    "url": "vendors~main.9a0bc239.iframe.bundle.js"
+  },
+  {
+    "revision": "df6626849911af9b2c90272e1af6bb30",
+    "url": "vendors~main.9a0bc239.iframe.bundle.js.LICENSE.txt"
+  }
+]);
